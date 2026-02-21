bd760bf4f0855968a6caa9b48dd04bdc640e67b7
kodeaki <kodeaki@proton.me>
2026-02-21 18:40:22 +0200

    added classes

--- cpp01/ex03/HumanB.hpp
+++ cpp01/ex03/HumanB.hpp
@@ -2,4 +2,19 @@
 #ifndef HUMANB_HPP
 # define HUMANB_HPP
 
+# include <string>
+# include <iostream>
+
+class HumanB{
+	private:
+		std::string	_name;
+		Weapon		_weapon;
+
+	public:
+		HumanB();
+		~HumanB();
+
+		void	attack();
+}
+
 #endif
--- cpp01/ex03/Weapon.hpp
+++ cpp01/ex03/Weapon.hpp
@@ -2,4 +2,18 @@
 #ifndef WEAPON_HPP
 # define WEAPON_HPP
 
+# include <string>
+
+class	Weapon{
+	private:
+		std::string	type;
+
+	public:
+		Weapon(std::string type);
+		~Weapon();
+
+		std::string const	getType();
+		void				setType(type);
+}
+
 #endif
